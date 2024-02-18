b02333461f20e7d90424e3020c8d6bc41f062fd6
rkarsnk <00_g1thub01@rkarsnk.net>
2024-02-18 20:30:18 +0900

    QEMU 起動スクリプトのmacOS対応

--- 01_clone_osbook.sh
+++ 01_clone_osbook.sh
@@ -6,5 +6,6 @@ fi
 if [ -d osbook ]; then
     cp osbook/devenv/make_image.sh osbook/devenv/make_image.sh.orig
     cp osbook/devenv/mount_image.sh osbook/devenv/mount_image.sh.orig
+    cp osbook/devenv/run_image.sh osbook/devenv/run_image.sh.orig
     cp devenv_mac/* osbook/devenv
 fi
