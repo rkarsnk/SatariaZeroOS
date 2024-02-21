3cbe674886800ad7ba4d1081391fbfeb990c0450
rkarsnk <00_g1thub01@rkarsnk.net>
2024-02-21 10:08:57 +0900

    clang-formatスタイルをMozillaに

--- MikanLoaderPkg/Main.c
+++ MikanLoaderPkg/Main.c
@@ -7,9 +7,10 @@
 #include <Protocol/SimpleFileSystem.h>
 #include <Uefi.h>
 
-struct MemoryMap {
+struct MemoryMap
+{
   UINTN buffer_size;
-  VOID *buffer;
+  VOID* buffer;
   UINTN map_size;
   UINTN map_key;
   UINTN descriptor_size;
@@ -20,7 +21,9 @@ struct MemoryMap {
 ポインタ引数mapを渡して，gBS->GetMemoryMap()を利用して，
 メモリマップを取得する．
 ----------------------------------------------------- */
-EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
+EFI_STATUS
+GetMemoryMap(struct MemoryMap* map)
+{
   if (map->buffer == NULL) {
     return EFI_BUFFER_TOO_SMALL;
   }
@@ -29,67 +32,73 @@ EFI_STATUS GetMemoryMap(struct MemoryMap *map) {
   /* ---------------------------------------------------------
   gBS->GetMemoryMap()は，UEFIブートサービスが提供するAPI．
   --------------------------------------------------------- */
-  return gBS->GetMemoryMap(&map->map_size, (EFI_MEMORY_DESCRIPTOR *)map->buffer,
-                           &map->map_key, &map->descriptor_size,
+  return gBS->GetMemoryMap(&map->map_size,
+                           (EFI_MEMORY_DESCRIPTOR*)map->buffer,
+                           &map->map_key,
+                           &map->descriptor_size,
                            &map->descriptor_version);
 }
 
 /* -------------------------------------------------------------
 メモリタイプの値からメモリタイプの名称を取得する．
 -------------------------------------------------------------- */
-const CHAR16 *GetMemoryTypeUnicode(EFI_MEMORY_TYPE type) {
+const CHAR16*
+GetMemoryTypeUnicode(EFI_MEMORY_TYPE type)
+{
   switch (type) {
-  case EfiReservedMemoryType:
-    return L"EfiReservedMemoryType";
-  case EfiLoaderCode:
-    return L"EfiLoaderCode";
-  case EfiLoaderData:
-    return L"EfiLoaderData";
-  case EfiBootServicesCode:
-    return L"EfiBootServicesCode";
-  case EfiBootServicesData:
-    return L"EfiBootServicesData";
-  case EfiRuntimeServicesCode:
-    return L"EfiRuntimeServicesCode";
-  case EfiRuntimeServicesData:
-    return L"EfiRuntimeServicesData";
-  case EfiConventionalMemory:
-    return L"EfiConventionalMemory";
-  case EfiUnusableMemory:
-    return L"EfiUnusableMemory";
-  case EfiACPIReclaimMemory:
-    return L"EfiACPIReclaimMemory";
-  case EfiACPIMemoryNVS:
-    return L"EfiACPIMemoryNVS";
-  case EfiMemoryMappedIO:
-    return L"EfiMemoryMappedIO";
-  case EfiMemoryMappedIOPortSpace:
-    return L"EfiMemoryMappedIOPortSpace";
-  case EfiPalCode:
-    return L"EfiPalCode";
-  case EfiPersistentMemory:
-    return L"EfiPersistentMemory";
-  case EfiMaxMemoryType:
-    return L"EfiMaxMemoryType";
-  default:
-    return L"InvalidMemoryType";
+    case EfiReservedMemoryType:
+      return L"EfiReservedMemoryType";
+    case EfiLoaderCode:
+      return L"EfiLoaderCode";
+    case EfiLoaderData:
+      return L"EfiLoaderData";
+    case EfiBootServicesCode:
+      return L"EfiBootServicesCode";
+    case EfiBootServicesData:
+      return L"EfiBootServicesData";
+    case EfiRuntimeServicesCode:
+      return L"EfiRuntimeServicesCode";
+    case EfiRuntimeServicesData:
+      return L"EfiRuntimeServicesData";
+    case EfiConventionalMemory:
+      return L"EfiConventionalMemory";
+    case EfiUnusableMemory:
+      return L"EfiUnusableMemory";
+    case EfiACPIReclaimMemory:
+      return L"EfiACPIReclaimMemory";
+    case EfiACPIMemoryNVS:
+      return L"EfiACPIMemoryNVS";
+    case EfiMemoryMappedIO:
+      return L"EfiMemoryMappedIO";
+    case EfiMemoryMappedIOPortSpace:
+      return L"EfiMemoryMappedIOPortSpace";
+    case EfiPalCode:
+      return L"EfiPalCode";
+    case EfiPersistentMemory:
+      return L"EfiPersistentMemory";
+    case EfiMaxMemoryType:
+      return L"EfiMaxMemoryType";
+    default:
+      return L"InvalidMemoryType";
   }
 }
-
 
 /* ----------------------------------------------------------------------
 メモリマップをfileで指定したファイルに書き出す
 ---------------------------------------------------------------------- */
-EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
+EFI_STATUS
+SaveMemoryMap(struct MemoryMap* map, EFI_FILE_PROTOCOL* file)
+{
   CHAR8 buf[256];
   UINTN len;
 
-  CHAR8 *header =
-      "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
+  CHAR8* header =
+    "Index, Type, Type(name), PhysicalStart, NumberOfPages, Attribute\n";
   len = AsciiStrLen(header);
   file->Write(file, &len, header);
 
-  Print(L"map->buffer = %08lx, map->map_size = %08lx\n", map->buffer,
+  Print(L"map->buffer = %08lx, map->map_size = %08lx\n",
+        map->buffer,
         map->map_size);
 
   EFI_PHYSICAL_ADDRESS iter;
@@ -97,10 +106,15 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
   for (iter = (EFI_PHYSICAL_ADDRESS)map->buffer, i = 0;
        iter < (EFI_PHYSICAL_ADDRESS)map->buffer + map->map_size;
        iter += map->descriptor_size, i++) {
-    EFI_MEMORY_DESCRIPTOR *desc = (EFI_MEMORY_DESCRIPTOR *)iter;
-    len = AsciiSPrint(buf, sizeof(buf), "%u, %x, %-ls, %08lx, %lx, %lx\n", i,
-                      desc->Type, GetMemoryTypeUnicode(desc->Type),
-                      desc->PhysicalStart, desc->NumberOfPages,
+    EFI_MEMORY_DESCRIPTOR* desc = (EFI_MEMORY_DESCRIPTOR*)iter;
+    len = AsciiSPrint(buf,
+                      sizeof(buf),
+                      "%u, %x, %-ls, %08lx, %lx, %lx\n",
+                      i,
+                      desc->Type,
+                      GetMemoryTypeUnicode(desc->Type),
+                      desc->PhysicalStart,
+                      desc->NumberOfPages,
                       desc->Attribute & 0xffffflu);
     file->Write(file, &len, buf);
   }
@@ -113,42 +127,54 @@ EFI_STATUS SaveMemoryMap(struct MemoryMap *map, EFI_FILE_PROTOCOL *file) {
 2. DeviceHandleからEfiSimpleFileSystemProtocol fsを取得する．
 3. fsからroot volumeを取得する．
 ------------------------------------------------------------------------ */
-EFI_STATUS OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL **root) {
-  EFI_LOADED_IMAGE_PROTOCOL *loaded_image;
-  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL *fs;
+EFI_STATUS
+OpenRootDir(EFI_HANDLE image_handle, EFI_FILE_PROTOCOL** root)
+{
+  EFI_LOADED_IMAGE_PROTOCOL* loaded_image;
+  EFI_SIMPLE_FILE_SYSTEM_PROTOCOL* fs;
 
-  gBS->OpenProtocol(image_handle, &gEfiLoadedImageProtocolGuid,
-                    (VOID **)&loaded_image, image_handle, NULL,
+  gBS->OpenProtocol(image_handle,
+                    &gEfiLoadedImageProtocolGuid,
+                    (VOID**)&loaded_image,
+                    image_handle,
+                    NULL,
                     EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
   gBS->OpenProtocol(loaded_image->DeviceHandle,
-                    &gEfiSimpleFileSystemProtocolGuid, (VOID **)&fs,
-                    image_handle, NULL, EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
+                    &gEfiSimpleFileSystemProtocolGuid,
+                    (VOID**)&fs,
+                    image_handle,
+                    NULL,
+                    EFI_OPEN_PROTOCOL_BY_HANDLE_PROTOCOL);
 
   fs->OpenVolume(fs, root);
 
   return EFI_SUCCESS;
 }
 
-EFI_STATUS EFIAPI UefiMain(EFI_HANDLE image_handle,
-                           EFI_SYSTEM_TABLE *system_table) {
+EFI_STATUS EFIAPI
+UefiMain(EFI_HANDLE image_handle, EFI_SYSTEM_TABLE* system_table)
+{
   Print(L"Hello, SatariaZero World!\n");
 
   // MemoryMap構造体の変数 memmapにUEFIブートサービスから
   // 取得したメモリマップを格納する．
   CHAR8 memmap_buf[4096 * 4];
-  struct MemoryMap memmap = {sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0};
+  struct MemoryMap memmap = { sizeof(memmap_buf), memmap_buf, 0, 0, 0, 0 };
   GetMemoryMap(&memmap);
 
   // ファイルを書き出すボリュームを開く．
-  EFI_FILE_PROTOCOL *root_dir;
+  EFI_FILE_PROTOCOL* root_dir;
   OpenRootDir(image_handle, &root_dir);
 
   // memmapファイルをOpenする
-  EFI_FILE_PROTOCOL *memmap_file;
-  root_dir->Open(
-      root_dir, &memmap_file, L"\\memmap.txt",
-      EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE | EFI_FILE_MODE_CREATE, 0);
+  EFI_FILE_PROTOCOL* memmap_file;
+  root_dir->Open(root_dir,
+                 &memmap_file,
+                 L"\\memmap.txt",
+                 EFI_FILE_MODE_READ | EFI_FILE_MODE_WRITE |
+                   EFI_FILE_MODE_CREATE,
+                 0);
 
   // memmapファイルに取得したメモリマップを書き出す．
   SaveMemoryMap(&memmap, memmap_file);
